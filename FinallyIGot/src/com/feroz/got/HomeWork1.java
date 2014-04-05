ec000f4b53520357f04aabe359772e147bb1e008
ashiftech <ashif2014dream@gmail.com>
2014-04-05 11:22:14 -0700

    New Home Work

---
+++ FinallyIGot/src/com/feroz/got/HomeWork1.java
@@ -0,0 +1,15 @@
+package com.feroz.got;
+
+public class HomeWork1 {
+
+	public static void main(String[] args) {
+	int Boys=17;
+	if(Boys==18){
+		System.out.println("Can go with girls");
+	}else{
+		System.out.println("go to hell");
+	}
+
+	}
+
+}
